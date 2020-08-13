f3db2e08c0f87bfa95d50b59f3d82d094e6e47e3
Sélène Desna <desna.selene@gmail.com>
2020-08-13 22:48:00 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+server_scripts {
+ 	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'Server.lua',
+
+ }
+ 
+client_scripts {
+ 	'Client.lua',
+ }
+
