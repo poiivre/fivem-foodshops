000b53c7d936464c65d8f89c026ef718b04792a5
Sélène Desna <desna.selene@gmail.com>
2020-09-09 19:07:18 +0200

    Adding Fruit Stand

--- Server.lua
+++ Server.lua
@@ -4,9 +4,9 @@
 
 ESX  = nil TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
----------------------
+----------------------
 --- Script Serveur ---
----------------------
+----------------------
 
 RegisterServerEvent('Selene_Cafe')
 AddEventHandler('Selene_Cafe', function( Item, Prix, Notif2, Notif3 )
@@ -23,6 +23,23 @@ AddEventHandler('Selene_Cafe', function( Item, Prix, Notif2, Notif3 )
 	end
 
 end)
+
+RegisterServerEvent('Selene_Stand')
+AddEventHandler('Selene_Stand', function( Item, Prix, Notif2, Notif3 )
+
+	local xPlayer = ESX.GetPlayerFromId(source)
+
+	if xPlayer.getMoney() >= Prix then
+		TriggerClientEvent('esx:showNotification', source, Notif2)							
+		TriggerClientEvent('Selene_Stand:Animation', source)
+		xPlayer.addInventoryItem(Item, 1)
+		xPlayer.removeMoney(Prix)						
+
+	else 	TriggerClientEvent('esx:showNotification', source, Notif3)		
+	end
+
+end)
+
 
 
 RegisterServerEvent('Selene_Fontaine')
